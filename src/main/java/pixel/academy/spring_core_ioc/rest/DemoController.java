b36b2ceaa981d3f897219246858e249423c0e760
Cojocari Dumitru <dimacojocar5i@gmail.com>
2026-09-01 20:56:56 +0200

    Comment constructor injection

--- src/main/java/pixel/academy/spring_core_ioc/rest/DemoController.java
+++ src/main/java/pixel/academy/spring_core_ioc/rest/DemoController.java
@@ -1,7 +1,9 @@
-package pixel.academy.spring_core_ioc;
+package pixel.academy.spring_core_ioc.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pixel.academy.spring_core_ioc.common.Chef;
 
 @RestController
 public class DemoController {
@@ -10,9 +12,11 @@ public class DemoController {
 
     // constructor
 
-    public DemoController(Chef theChef) {
-        myChef = theChef;
-    }
+
+//    @Autowired
+//    public DemoController(Chef theChef) {
+//        myChef = theChef;
+//    }
 
     @GetMapping("/dailyrecipe")
     public String getDailyRecipe() {
